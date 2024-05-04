581153b5985b9d31cb4dd7ff9855ad2d26c02eb5
falyArivelo <119029443+falyArivelo@users.noreply.github.com>
2024-05-04 18:34:39 +0300

    commit image lesson

--- src/components/Lesson/index.jsx
+++ src/components/Lesson/index.jsx
@@ -6,7 +6,7 @@ const Index = () => {
             <div className="chapitres">
                 <div className="chapitre">
                     <div className="image">
-                        <img src={process.env.REACT_APP_REPOSITORY + "/assets/images/pulse.jpg"} alt="" />
+                        <img src={process.env.REACT_APP_REPOSITORY + "/assets/images/language.jpg"} alt="" />
                     </div>
                     <div className="infos">
                         <div className="title">Langue et communication</div>
@@ -15,7 +15,8 @@ const Index = () => {
                 </div>
                 <div className="chapitre">
                     <div className="image">
-                        <img src="" alt="ima" />
+                        <img src={process.env.REACT_APP_REPOSITORY + "/assets/images/culture.jpg"} alt="" />
+
                     </div>
                     <div className="infos">
                         <div className="title">Culture et coutumes </div>
@@ -24,7 +25,8 @@ const Index = () => {
                 </div>
                 <div className="chapitre">
                     <div className="image">
-                        <img src="" alt="ima" />
+                        <img src={process.env.REACT_APP_REPOSITORY + "/assets/images/emotion.jpg"} alt="" />
+
                     </div>
                     <div className="infos">
                         <div className="title">Émotions et comportements</div>
@@ -33,51 +35,14 @@ const Index = () => {
                 </div>
                 <div className="chapitre">
                     <div className="image">
-                        <img src="" alt="ima" />
+                        <img src={process.env.REACT_APP_REPOSITORY + "/assets/images/higiene.jpg"} alt="" />
+
                     </div>
                     <div className="infos">
                         <div className="title">Hygiène personnelle</div>
                         <div className="libelle">Comprendre l'importance de l'hygiène personnelle et apprendre les habitudes de propreté pour maintenir une apparence soignée</div>
                     </div>
                 </div>
-                <div className="chapitre">
-                    <div className="image">
-                        <img src="" alt="ima" />
-                    </div>
-                    <div className="infos">
-                        <div className="title">Empathie et compassion</div>
-                        <div className="libelle">Développer l'empathie et la compassion envers les autres, et apprendre à respecter leurs sentiments et leurs besoins</div>
-                    </div>
-                </div>
-
-                <div className="chapitre">
-                    <div className="image">
-                        <img src="" alt="ima" />
-                    </div>
-                    <div className="infos">
-                        <div className="title">Éthique et valeurs</div>
-                        <div className="libelle"> Comprendre les concepts d'éthique et de valeurs humaines, et adopter des comportements moralement acceptables dans la société.</div>
-                    </div>
-                </div>
-                <div className="chapitre">
-                    <div className="image">
-                        <img src="" alt="ima" />
-                    </div>
-                    <div className="infos">
-                        <div className="title">Éducation et formation</div>
-                        <div className="libelle">Acquérir des connaissances et des compétences par le biais de l'éducation et de la formation, afin de contribuer de manière positive à la société</div>
-                    </div>
-                </div>
-                <div className="chapitre">
-                    <div className="image">
-                        <img src="" alt="ima" />
-                    </div>
-                    <div className="infos">
-                        <div className="title">Coexistence pacifique</div>
-                        <div className="libelle">Apprendre à coexister pacifiquement avec les humains en évitant les conflits et en favorisant la compréhension mutuelle</div>
-                    </div>
-                </div>
-
             </div>
         </div>
     )
